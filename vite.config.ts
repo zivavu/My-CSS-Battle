1171bee646e381482c2bcb0f86934fd45b0286f4
Tomasz <tomasz.kierzenkowski@capgemini.com>
2026-09-08 11:47:06 +0200

    Tests setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+/// <reference types="vitest/config" />
 
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
+
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
+
   plugins: [react(), tailwindcss()],
+
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+  },
+
   build: {
     chunkSizeWarningLimit: 1000,
   },
-})
+});
